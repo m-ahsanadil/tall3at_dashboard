37e8b07057dc241fbabe04db1341ee53800a8abe
ahmad01213 <aebrahima830@gmail.com>
2025-09-15 16:42:56 +0300

    devwebapi tall3at dashboard...

--- src/constants/config.js
+++ src/constants/config.js
@@ -39,10 +39,7 @@ export const GENDER_OPTIONS = [
 
 // API Configuration
 export const API_CONFIG = {
-  // BASE_URL: 'https://webapi.tall3at.com',
-  // BASE_URL: 'https://9f1f1a23cfa5.ngrok-free.app',
-  // BASE_URL: 'https://devwebapi.tall3at.com',
-  BASE_URL: 'http://localhost:5030',
+  BASE_URL: 'https://devwebapi.tall3at.com',
   TIMEOUT: 10000,
 };
 
